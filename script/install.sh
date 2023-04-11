027aabe7a07855d120948b8f4bd5ae2c7a0c6212
h0ru <117091833+h0ru@users.noreply.github.com>
2023-04-11 17:16:50 -0300

    Create install.sh

---
+++ script/install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#Colors:
+        Rd='\033[0;31m'    # Red
+        Be='\033[0;34m'   # Blue
+        Gn='\033[0;32m'  # Green
+        We='\033[0;97m'  # White
+
+if [ "$(id -u)" != "0" ]; then
+    echo -e "
+    $Rd Run with superuser privileges (sudo)! $We
+    " >&2
+    exit 1
+fi
+
+#docker
+if [ -x "$(command -v docker)" ]; then
+    echo -e "
+    [*]$Be docker is already installed on your machine! $We
+    "
+else
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io
+    usermod -aG docker $USER
+    echo -e "
+    [*]$Gn docker has been installed on your machine! $We
+    "
+fi
+
+#wget
+if [ -x "$(command -v wget)" ]; then
+    echo -e "
+    [*]$Be wget is already installed on your machine! $We
+    "
+else
+    apt-get install wget
+    echo -e "
+    [*]$Gn wget has been installed on your machine! $We
+    "
+fi
+
+#sudocker
+if [ -x "$(command -v sudocker)" ]; then
+    echo -e "
+    [*]$Be sudocker is already installed on your machine! $We
+    "
+else
+    wget -q https://raw.githubusercontent.com/h0ru/sudocker/main/script/sudocker -O /usr/local/bin/sudocker
+    echo -e "
+    [*]$Gn sudocker has been installed on your machine! $We
+    "
+fi
