f3f618f7a9bc19f7358b17cf30c7e777d7ca5b74
h0ru <117091833+h0ru@users.noreply.github.com>
2023-04-11 17:19:56 -0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -13,39 +13,42 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-#docker
+#Docker
 if [ -x "$(command -v docker)" ]; then
     echo -e "
     [*]$Be docker is already installed on your machine! $We
     "
 else
     apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io
+    apt-get -y install docker-ce docker-ce-cli containerd.io
     usermod -aG docker $USER
+    clear
     echo -e "
     [*]$Gn docker has been installed on your machine! $We
     "
 fi
 
-#wget
+#Wget
 if [ -x "$(command -v wget)" ]; then
     echo -e "
     [*]$Be wget is already installed on your machine! $We
     "
 else
-    apt-get install wget
+    apt-get -y install wget
+    clear
     echo -e "
     [*]$Gn wget has been installed on your machine! $We
     "
 fi
 
-#sudocker
+#Sudocker
 if [ -x "$(command -v sudocker)" ]; then
     echo -e "
     [*]$Be sudocker is already installed on your machine! $We
     "
 else
-    wget -q https://raw.githubusercontent.com/h0ru/sudocker/main/script/sudocker -O /usr/local/bin/sudocker
+    wget -q https://raw.githubusercontent.com/h0ru/sudocker/main/script/sudocker -O /usr/local/bin/sudocker ; chmod +x /usr/local/bin/sudocker
+    clear
     echo -e "
     [*]$Gn sudocker has been installed on your machine! $We
     "
